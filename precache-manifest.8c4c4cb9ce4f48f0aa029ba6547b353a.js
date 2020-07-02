3e57d114c49524491d97a2ba6a95e4fc4ccb545d
Geunu <upry4318@naver.com>
2020-07-02 23:44:15 +0900

    Updates

---
+++ precache-manifest.8c4c4cb9ce4f48f0aa029ba6547b353a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbc93740959511b44e6544380fbaa334",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "bffbe0c35fa696568337",
+    "url": "/movie_app_2020/static/css/main.ffc78184.chunk.css"
+  },
+  {
+    "revision": "b17fb38856ffb36d44ef",
+    "url": "/movie_app_2020/static/js/2.14c54020.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie_app_2020/static/js/2.14c54020.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bffbe0c35fa696568337",
+    "url": "/movie_app_2020/static/js/main.3e5905be.chunk.js"
+  },
+  {
+    "revision": "86e4940ccaeed0f37e85",
+    "url": "/movie_app_2020/static/js/runtime-main.83f6590a.js"
+  }
+]);
